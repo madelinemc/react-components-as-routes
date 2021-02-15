139f8fb0a72e71e43d6eab6cfe1e3731dab3117c
madelinemc <madeline.coons@gmail.com>
2021-02-15 13:28:44 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,162 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// // Step 1. Import react-router functions
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+
+// //Step 3. next, add components for About and Login: 
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>This is my about component!</h1>
+//     </div>
+//   );
+// };
+
+// const Login = () => {
+//   return (
+//     <div>
+//       <form>
+//         <div>
+//           <input type="text" name="username" placeholder="Username" />
+//           <label htmlFor="username">Username</label>
+//         </div>
+//         <div>
+//           <input type="password" name="password" placeholder="Password" />
+//           <label htmlFor="password">Password</label>
+//         </div>
+//         <input type="submit" value="Login" />
+//       </form>
+//     </div>
+//   );
+// };
+
+// // Step 2. Changed to have router coordinate what is displayed
+// ReactDOM.render((
+//   <Router>
+//     <div>
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/about" component={About} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </Router>),
+//   document.getElementById('root')
+// );
+//The Router (our alias for BrowserRouter) component is the base for our application's routing. 
+//It is where we declare how React Router will be used. 
+//Notice that nested inside the Router component we use the Route component. 
+//The Route component has two props in our example: path and component. 
+//The Route component is in charge of saying: "when the URL matches this specified path, render this specified component".
+
+
+
+//FULL IMPLEMENTATION OF CODE ALONG WITH NAVLINK COMPONENTS: 
 import React from 'react';
 import ReactDOM from 'react-dom';
+/* Add NavLink to importer */
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
-  return (
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+const Home = () => <h1>Home!</h1>;
+
+const About = () => <h1>This is my about component!</h1>;
+
+const Login = () =>
+  <form>
+    <h1>Login</h1>
     <div>
-      <h1>Home!</h1>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Login" />
+  </form>;
 
-ReactDOM.render(
-  <Home />,
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
+
+
+//TO REFACTOR: each item (home, about, login, navbar) should be broken out into its own file and made into React.Component
+//with below as the index:
+
+// src/index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Home from './Home'
+// import About from './About'
+// import Login from './Login'
+// import Navbar from './Navbar'
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// ReactDOM.render((
+//   <Router>
+//     <div>
+//       <Navbar />
+//       <Route exact path="/" component={Home} />
+//       <Route exact path="/about" component={About} />
+//       <Route exact path="/login" component={Login} />
+//     </div>
+//   </Router>),
+//   document.getElementById('root')
+// );
